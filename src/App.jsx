36550e6a95bc7f0688b12e7d1fc9ab1a12dac19f
Anastasiia Yemets <anastasiaemec40@gmail.com>
2025-01-19 17:28:17 +0100

    lesson2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import './App.css';
+import Lesson02 from './lessons/Lesson02/Lesson02';
+
+
+
 
 function App() {
 
-  return <div className='app'>Hello, React</div>
-  
+  return (<div className='app'>
+    <Lesson02/>
+    </div>
+    );
 }
 
 export default App;
+
