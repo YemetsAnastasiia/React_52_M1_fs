4aa528a686c66575c4cc10a0a5a2a9f3c1053768
Anastasiia Yemets <anastasiaemec40@gmail.com>
2025-01-19 18:37:53 +0100

    homework_02

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Homework_02 from './homeworks/homework_02/homework_02';
 import Lesson02 from './lessons/Lesson02/Lesson02';
 
 
@@ -7,7 +8,8 @@ import Lesson02 from './lessons/Lesson02/Lesson02';
 function App() {
 
   return (<div className='app'>
-    <Lesson02/>
+   {/*  <Lesson02/> */}
+   <Homework_02 />
     </div>
     );
 }
